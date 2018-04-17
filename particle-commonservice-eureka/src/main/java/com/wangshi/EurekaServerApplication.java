bb8a5e6527c8f949693c097c2aa5ee3fe1aef27f
wangjian <wangjian@jsanyun.com>
2018-04-17 17:38:30 +0800

    Initial commit

---
+++ particle-commonservice-eureka/src/main/java/com/wangshi/EurekaServerApplication.java
@@ -0,0 +1,25 @@
+/*
+ * ==============================================================
+ * Copyright (c) 2012 ~ 2018. anyuncloud.com All rights reserved.
+ *                                           Created by wangjian
+ * ==============================================================
+ */
+
+package com.wangshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by wangjian
+ * Date: 18-4-17
+ * Time: 下午1:41
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
