49437964c643b4fc18447cef3f27f599d3d7f987
wangjian <wangjian@jsanyun.com>
2018-04-18 14:07:16 +0800

    创建子项目eureka消费端

---
+++ eureka-consumer/src/main/java/wangshi/EurekaConsumerApplication.java
@@ -0,0 +1,33 @@
+/*
+ * ==============================================================
+ * Copyright (c) 2012 ~ 2018. anyuncloud.com All rights reserved.
+ *                                           Created by wangjian
+ * ==============================================================
+ */
+
+package wangshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by wangjian
+ * Date: 18-4-17
+ * Time: 下午5:46
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaConsumerApplication {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConsumerApplication.class, args);
+    }
+}
