c1c882136b945aae6bcd58437969c456c14230c3
wangjian <wangjian@jsanyun.com>
2018-04-18 14:06:44 +0800

    创建子项目eureka客户端之一

---
+++ eureka-client/src/main/java/com/wangshi/EurekaClientApplication.java
@@ -0,0 +1,26 @@
+/*
+ * ==============================================================
+ * Copyright (c) 2012 ~ 2018. anyuncloud.com All rights reserved.
+ *                                           Created by wangjian
+ * ==============================================================
+ */
+
+package com.wangshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by wangjian
+ * Date: 18-4-17
+ * Time: 下午5:46
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
---
+++ eureka-client/src/main/java/com/wangshi/controller/DcController.java
@@ -0,0 +1,33 @@
+/*
+ * ==============================================================
+ * Copyright (c) 2012 ~ 2018. anyuncloud.com All rights reserved.
+ *                                           Created by wangjian
+ * ==============================================================
+ */
+
+package com.wangshi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wangjian
+ * Date: 18-4-18
+ * Time: 上午10:05
+ */
+@RestController
+public class DcController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping(value = "/dc")
+    public String dc(){
+        String services = "Service: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+}
