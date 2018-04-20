592e62f465094b0fe10a6f7ecb4a786dc14da189
wangjian <wangjian@jsanyun.com>
2018-04-20 14:30:44 +0800

    创建子项目config-client客户端从配置中心获取git方案

--- config-client/src/main/java/com/wangshi/GetGitConfigApplication.java
+++ config-client/src/main/java/com/wangshi/GetGitConfigApplication.java
@@ -9,17 +9,15 @@ package com.wangshi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by wangjian
  * Date: 18-4-20
  * Time: 上午10:40
  */
-@EnableConfigServer
 @SpringBootApplication
-public class GitConfigApplication {
+public class GetGitConfigApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GitConfigApplication.class, args);
+        SpringApplication.run(GetGitConfigApplication.class, args);
     }
 }
