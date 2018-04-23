42a3a6ed98508fc0dd56dc6aacc5bd2063ca3144
wangjian <wangjian@jsanyun.com>
2018-04-23 16:55:57 +0800

    创建Hystrix监控面板

---
+++ hystrix-dashboard/src/main/java/com/wangshi/HystrixDashboardApplication.java
@@ -0,0 +1,26 @@
+/*
+ * ==============================================================
+ * Copyright (c) 2012 ~ 2018. anyuncloud.com All rights reserved.
+ *                                           Created by wangjian
+ * ==============================================================
+ */
+
+package com.wangshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by wangjian
+ * Date: 18-4-23
+ * Time: 下午4:29
+ */
+
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
