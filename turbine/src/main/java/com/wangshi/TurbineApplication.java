36ef8a7ce5df5ebb1ee52dec776392467d3fe58d
wangjian <wangjian@jsanyun.com>
2018-04-24 14:33:35 +0800

    创建turbine对服务的Hystrix数据进行聚合展示

---
+++ turbine/src/main/java/com/wangshi/TurbineApplication.java
@@ -0,0 +1,32 @@
+/*
+ * ==============================================================
+ * Copyright (c) 2012 ~ 2018. anyuncloud.com All rights reserved.
+ *                                           Created by wangjian
+ * ==============================================================
+ */
+
+package com.wangshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by wangjian
+ * Date: 18-4-23
+ * Time: 下午5:25
+ */
+
+@Configuration
+@EnableAutoConfiguration
+@EnableTurbine
+@EnableDiscoveryClient
+public class TurbineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApplication.class, args);
+    }
+}
